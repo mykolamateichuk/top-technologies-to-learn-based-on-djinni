b2910cbf2a06fb917d6ec62366ac859620486b4f
mykolamateichuk <mateichukmykola@gmail.com>
2024-01-09 20:52:59 +0200

    Add detail info parsing using selenium

--- parser/djinni_vacancies_parser/spiders/djinni.py
+++ parser/djinni_vacancies_parser/spiders/djinni.py
@@ -1,5 +1,10 @@
 import scrapy
+from scrapy import Selector
 from scrapy.http import Response
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+from ..technologies import TECHNOLOGIES
 
 ENGLISH_LEVELS_DJINNI = [
     "No English",
@@ -24,6 +29,13 @@ class DjinniSpider(scrapy.Spider):
     allowed_domains = ["djinni.co"]
     start_urls = ["https://djinni.co/jobs/?primary_keyword=Python"]
 
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.driver = webdriver.Chrome()
+
+    def close(self, reason):
+        self.driver.close()
+
     def parse(self, response: Response, **kwargs):
         for vacancy in response.css("main .list-jobs .list-jobs__item"):
             english_level = (vacancy
@@ -36,11 +48,23 @@ class DjinniSpider(scrapy.Spider):
                                 .get()
                                 .strip())
 
+            views_and_applications = (vacancy.css(".text-muted .nobr")[1]
+                                      .css(".mr-2::attr(title)").getall())
+
+            number_of_views = int(views_and_applications[0].split()[0])
+            number_of_applications = int(views_and_applications[1].split()[0])
+
+            percentage_of_applications = round(
+               number_of_applications / number_of_views, 2
+            ) * 100
+
             if english_level not in ENGLISH_LEVELS_DJINNI:
                 english_level = "Not stated"
 
             experience_years = (EXPERIENCE_YEARS_DJINNI
                                 .get(experience_years, "Not stated"))
+
+            detail_data = self.__parse_detailed_vacancy(response, vacancy)
 
             yield {
                 "vacancy_name": (vacancy.css(".job-list-item__link::text")
@@ -50,9 +74,12 @@ class DjinniSpider(scrapy.Spider):
                                   .get()
                                   .strip()
                                   .replace("\t", "")
-                                  .replace("\n", "")),
+                                  .replace("\n", "")
+                                  .replace("    ", " ")),
                 "english_level": english_level,
                 "experience_years": experience_years,
+                "percentage_of_applications": int(percentage_of_applications),
+                "technologies_mentioned": detail_data["technologies_mentioned"]
             }
 
         next_page = (response
@@ -62,3 +89,27 @@ class DjinniSpider(scrapy.Spider):
         if next_page != "#":
             next_page_url = response.urljoin(next_page)
             yield scrapy.Request(next_page_url, callback=self.parse)
+
+    def __parse_detailed_vacancy(self, response: Response, vacancy: Selector):
+        detailed_url = response.urljoin(vacancy.css(".job-list-item__link::attr(href)").get())
+        self.driver.get(detailed_url)
+
+        named_technologies = []
+
+        stated_technologies = [tech.text for tech in self.driver.find_elements(
+            By.CSS_SELECTOR, ".job-additional-info--item-text"
+        )[1].find_elements(By.CSS_SELECTOR, "span")]
+
+        named_technologies += stated_technologies
+
+        vacancy_text = self.driver.find_element(By.CSS_SELECTOR, ".row-mobile-order-2").text
+
+        for category in TECHNOLOGIES.keys():
+            for item_name, item_value in TECHNOLOGIES[category].items():
+                for name in item_value:
+                    if vacancy_text.find(name) != -1:
+                        named_technologies.append(item_name)
+
+        return {
+           "technologies_mentioned": list(set(named_technologies))
+        }
---
+++ parser/djinni_vacancies_parser/technologies.py
@@ -0,0 +1,44 @@
+TECHNOLOGIES = {
+    "Languages": {
+        "Python": ["Python", "python"],
+        "JavaScript": ["JavaScript", "javascript", "JS"],
+        "HTML": ["HTML", "html"],
+        "CSS": ["CSS", "css"]
+    },
+    "Frameworks": {
+        "Django": ["Django"],
+        "Django REST Framework": ["DRF", "Django REST Framework"],
+        "Flask": ["Flask"],
+        "FastAPI": ["FastAPI"],
+        "Pyramid": ["Pyramid"],
+        "ReactJS": ["ReactJS", "React"],
+        "Vue": ["Vue", "vue"]
+    },
+    "Databases": {
+        "PostgreSQL": ["PostgreSQL"],
+        "SQLite": ["SQLite", "SQLite3"],
+        "Redis": ["Redis"],
+    },
+    "VCS": {
+        "Git": ["Git", "git", "GitHub"],
+        "BitBucket": ["BitBucket", "bitbucket"],
+    },
+    "Libraries": {
+        "Numpy": ["Numpy", "numpy"],
+        "Pandas": ["Pandas", "pandas"],
+        "aiohttp": ["aiohttp"],
+        "matplotlib": ["matplotlib"]
+    },
+    "Other": {
+        "Celery": ["Celery", "celery"],
+        "REST": ["REST", "RESTful"],
+        "OOP": ["OOP", "oop"],
+        "JWT": ["JWT"],
+        "SQL": ["SQL", "sql"],
+        "NoSQL": ["NoSQL", "nosql"],
+        "Docker": ["Docker", "docker"],
+        "Linux": ["Linux", "linux"],
+        "CI/CD": ["CI/CD", "ci/cd"],
+        "HTTP": ["HTTP", "http"],
+    }
+}
